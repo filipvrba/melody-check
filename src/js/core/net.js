fece5160f92eb6fb18aaaf31c7431f94a82bcc8d
Filip Vrba <filipvrba@pm.me>
2025-01-27 11:11:15 +0100

    update net class

--- src/js/core/net.js
+++ src/js/core/net.js
@@ -1,9 +1,47 @@
+import { ENV } from "../env";
+
 export default class Net {
   static curl(url, callback) {
     return fetch(url).then(response => response.text()).then((text) => {
       if (callback) return callback(text)
+    }).catch(() => {
+      if (callback) return callback(null)
+    })
+  };
+
+  static bef(query, callback) {
+    let dbName = ENV.VITE_DATABASE;
+    let encodeQuery = encodeURIComponent(query);
+
+    return Net.curl(
+      `/api/bef?db=${dbName}&query=${encodeQuery}`,
+
+      (response) => {
+        let data;
+
+        if (response) {
+          data = JSON.parse(response);
+
+          if (data.status_code === 403 || data.status_code === 405 || data.status === "SQL Error") {
+            console.warn(data);
+            if (callback) return callback(null)
+          } else if (callback) {
+            return callback(data)
+          }
+        } else if (callback) {
+          return callback(null)
+        }
+      }
+    )
+  };
+
+  static checkInternet(callback) {
+    let healthUrl = "https://bef.fly.dev/health";
+
+    return fetch(healthUrl, {method: "GET"}).then((response) => {
+      if (callback) return callback(true)
+    }).catch(() => {
+      if (callback) return callback(false)
     })
   }
-};
-
-window.Net = Net
+}
--- src/rb/core/net.js.rb
+++ src/rb/core/net.js.rb
@@ -1,3 +1,5 @@
+import ['ENV'], '../env'
+
 export default class Net
   def self.curl(url, &callback)
     fetch(url)
@@ -7,6 +9,43 @@ export default class Net
     .then(lambda do |text|
       callback(text) if callback
     end)
+    .catch(lambda do
+      callback(nil) if callback
+    end)
+  end
+
+  def self.bef(query, &callback)
+    db_name = ENV::VITE_DATABASE
+
+    encode_query = encodeURIComponent(query)
+    Net.curl("/api/bef?db=#{db_name}&query=#{encode_query}") do |response|
+
+      if response
+        data = JSON.parse(response)
+        if data['status_code'] == 403 || data['status_code'] == 405 ||
+          data.status == 'SQL Error'
+          
+          console.warn(data)
+          callback(nil) if callback
+        else
+          callback(data) if callback
+        end
+      else
+        callback(nil) if callback
+      end
+
+    end
+  end
+
+  def self.check_internet(&callback)
+    health_url = 'https://bef.fly.dev/health'
+
+    fetch(health_url, { method: 'GET' })
+    .then(lambda do |response|
+      callback(true) if callback
+    end)
+    .catch(lambda do
+      callback(false) if callback
+    end)
   end
 end
-window.Net = Net
