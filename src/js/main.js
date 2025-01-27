843a3b3cf8a9fe1c7851feee436e015efe12b921
Filip Vrba <filipvrba@pm.me>
2025-01-27 10:54:37 +0100

    init bootstrap css

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+import "../css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
 import "../css/style.css";
 import "./core";
 document.querySelector("#app").innerHTML = "<h1>Hello RubyJS</h1>"
